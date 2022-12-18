1955f71ffef25344b79c30f86189a470f658ee5d
Jeka Shiyan <ncringe31@gmail.com>
2022-12-18 12:19:40 +0200

    corsWhitelist

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ import errorMiddleware from './middlewares/error-middleware.js';
 dotenv.config()
 
 const PORT = process.env.PORT || 5000;
-const app = express()
+const app = express();
+
+const corsWhitelist = [
+    process.env.CLIENT_URL,
+    "http://localhost:3000"
+]
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL
+    origin: corsWhitelist,
 }));
 app.use('/api', router);
 app.use(errorMiddleware);
