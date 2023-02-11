1f6416d00e0048a17f7b9dcedd50dbb47c646766
Shiyan7 <jeka.shiyan@gmail.com>
2023-02-11 17:57:23 +0200

    fix at

--- service/token-service.js
+++ service/token-service.js
@@ -3,7 +3,7 @@ import tokenModel from '../models/token-model.js';
 
 class TokenService {
     generateTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: 60 * 60 * 24 })
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {exp: 60 * 60 * 24 })
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
         return {
             accessToken,
